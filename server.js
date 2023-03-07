ba753081da4d789e806af37d2202c9e176c69d8e
Jena <anwjena@deloitte.com>
2023-03-07 17:32:50 +0530

    updating host

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const hostname = '35.175.104.153';
-const port = 3000;
+const port = 8010;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
